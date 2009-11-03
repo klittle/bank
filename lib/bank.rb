c2242dd6da36b304554dbdaaf8c5b80297123dea
Kathy Little <klittle@edaya.com>
2009-11-03 15:19:37 -0800

    added name search method

--- lib/bank.rb
+++ lib/bank.rb
@@ -5,9 +5,9 @@
 require 'account.rb'
 
 class Bank
-  
-#include account
-  
+
+  #include account
+
   attr_reader :accounts, :bank_name, :id_number
 
   def initialize(bank_name)
@@ -22,48 +22,34 @@ class Bank
     @accounts[@id_number] = new_account
     return @id_number
   end
-  
+
   def bank_deposit(account_no, deposit_amount)
     current_account = @accounts[account_no]
     return current_account.deposit(deposit_amount)
-    #return 25115
+    return 25115
   end
 
-  # def access_accounts
-  # #     Account::initialize
-  # end
+  def bank_withdrawal(account_no, withdraw_amt)
+    current_account = @accounts[account_no]
+    return current_account.withdraw(withdraw_amt)
+    return 10000
+  end
+
+  def zzget_hash_accounts_key(name)
+    @accounts.each {|k, v|
+      puts "in get accounts key"
+      puts "key = #{k}, value = #{v.name}"
+      if (v.name == name)
+        return k
+      end
+    }
+    return 0
+  end
 
 end
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def make_new_account
-#   @account_name = {
-#     1 => "Mary Smith",
-#     2 => "Jim Hopper",
-#     3 => "Sally Supper",
-#     4 => "Tom Clancy"}
-
-  
   
   # @account_name.each {|key, value| puts "#{value} has Account number #{key}"}
   #   account_amount.each {|key, value| puts "#{key} has Account number #{value}"}
--- test/test_bank.rb
+++ test/test_bank.rb
@@ -20,7 +20,7 @@ class TestBank < Test::Unit::TestCase
   end
 
   def test_make_new_account
-    puts "in test_amake_new_account"
+    puts "in test_make_new_account"
     @account_no = @new_bank.make_new_account("Tim Small", 25000)
     puts "@account_no = #{@account_no}"
     current_account = @new_bank.accounts[@account_no]
@@ -46,6 +46,43 @@ class TestBank < Test::Unit::TestCase
     puts "new_balance = #{new_balance}"
     assert_equal new_balance, @new_bank.bank_deposit(@account_no, deposit_amt)
   end
+  
+  def test_bank_withdrawal
+    puts "in test_bank_withdrawal"
+    @account_no = @new_bank.make_new_account("Mary Pepper", 15000)
+       
+    current_account = @new_bank.accounts[@account_no]
+    puts "@account_no = #{@account_no}"
+    puts "@current_account.class = #{@current_account.class}"
+    current_balance = current_account.balance
+    puts "current_balance = #{current_balance}"
+    withdraw_amt = 5000
+    new_balance = current_balance - withdraw_amt
+    puts "new_balance = #{new_balance}"
+    assert_equal new_balance, @new_bank.bank_withdrawal(@account_no, withdraw_amt)
+  end
+  
+ # def test_zadd_clients
+ #    @new_bank.make_new_account("Mary Pepper", 15000)
+ #    @new_bank.make_new_account("Mary Smith", 25000)
+ #    @new_bank.make_new_account("Jim Hopper", 10000)
+ #    @new_bank.make_new_account("Tom Clancy", 5000)
+ #    @new_bank.make_new_account("Harold Salt", 1000)
+ #    @new_bank.make_new_account("Jim Jones", 50)
+ #  # @new_bank.accounts.each {|key, value| puts "#{value} has Account number #{key}"}
+ #   #@new_bank.accounts.each {|key, value| puts "#{key} has #{value}"}
+ # end
+ 
+ def test_zzget_hash_accounts_key
+   @account_no = @new_bank.make_new_account("Mary Pepper", 15000)
+   @account_no = @new_bank.make_new_account("Mary Smith", 25000)
+   @account_no = @new_bank.make_new_account("Jim Hopper", 10000)
+   @account_no = @new_bank.make_new_account("Tom Clancy", 5000)
+   @account_no = @new_bank.make_new_account("Harold Salt", 1000)
+   @account_no = @new_bank.make_new_account("Jim Jones", 50)
+   assert_equal 4, @new_bank.zzget_hash_accounts_key("Tom Clancy")
+ end
+   
 
   # def test_deposit_to_acct_unknown_person
   #   flunk "write me"
